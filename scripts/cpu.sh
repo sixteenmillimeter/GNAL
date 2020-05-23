5085b8bb48dac7084d2a7f837c9c342173ee07fb
mmcwilliams <hi@mmcwilliams.com>
2020-05-23 00:12:28 -0400

    Add linux cpu functionality to eponymous script

--- scripts/cpu.sh
+++ scripts/cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-   CPU=-1
+   CPU=`cat /proc/cpuinfo | grep "model name" | head -n 1 | awk '{for (i=4; i<=NF; i++) print $i}'`
 elif [[ "$OSTYPE" == "darwin"* ]]; then
    CPU=`sysctl -a | grep machdep.cpu.brand_string | awk '{for (i=2; i<=NF; i++) print $i}'`
 else
